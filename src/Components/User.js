c155862364e836a7866bfb753c835e4a455abd22
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2021-10-19 21:24:03 +0330

    add username and count of  follower following and playlist

--- src/Components/User.js
+++ src/Components/User.js
@@ -5,6 +5,7 @@ import { getUserInfo, logout } from "../Apis/Spotify";
 import { catchErrors } from "../utils";
 import { IconUser, IconInfo } from "../Icons";
 import Loader from "react-loader-spinner";
+import { Link } from "@reach/router";
 
 const User = () => {
   const [user, setUser] = useState(null);
@@ -27,6 +28,8 @@ const User = () => {
     };
     catchErrors(fetchData());
   }, []);
+  const totalPlaylists = playlists ? playlists.total : 0;
+
   return (
     <React.Fragment>
       {user ? (
@@ -41,12 +44,34 @@ const User = () => {
                 </div>
               )}
             </div>
-            <div
-              className="diUserName"
+            <a
+              className="UserName"
               href={user.external_urls.spotify}
               target="_blank"
               rel="noopener noreferrer">
               <div className="Name">{user.display_name}</div>
+            </a>{" "}
+            <div className=" Stats">
+              <div className=" Stat">
+                <div className=" Number">{user.followers.total}</div>
+                <div className=" NumLabel">Followers</div>
+              </div>
+              {followedArtists && (
+                <div className=" Stat">
+                  <div className=" Number">
+                    {followedArtists.artists.items.length}
+                  </div>
+                  <div className=" NumLabel">Following</div>
+                </div>
+              )}
+              {totalPlaylists && (
+                <div className=" Stat">
+                  <Link className="decor" to="playlists">
+                    <div className=" Number">{totalPlaylists}</div>
+                    <div className=" NumLabel">Playlists</div>
+                  </Link>
+                </div>
+              )}
             </div>
             <button className="LogoutButton" onClick={handleLogOutClick}>
               logout
